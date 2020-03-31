2ee7b4abc36839515e102a47932f5b69f955e6af
markziemann <mark.ziemann@gmail.com>
2020-04-01 01:29:52 +1100

    Update files and readme

--- bioinfo_asst3_part2_files/create_fasta.sh
+++ bioinfo_asst3_part2_files/create_fasta.sh
@@ -8,5 +8,5 @@ gunzip -kf Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz
 
 cut -d ' ' -f1 Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa \
 | perl unwrap_fasta.pl - - \
-| paste - - | shuf | head -20 | tr '\t' '\n' | tee sample_named.fa \
+| paste - - | shuf | head -100 | tr '\t' '\n' | tee sample_named.fa \
 | grep -v '>' | nl -n ln | sed 's/^/>/' | tr '\t' '\n' > sample.fa
