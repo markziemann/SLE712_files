c6778853339e941de549f34e90ce31d3aba05531
markziemann <mark.ziemann@gmail.com>
2020-04-20 03:13:45 +1000

    Let's learn apply :)

--- bioinfo_asst3_part2_files/mutblast_functions.R
+++ bioinfo_asst3_part2_files/mutblast_functions.R
@@ -4,9 +4,31 @@ library("rBLAST")
 
 ##########################################################
 # myblastn is a wrapper for the unix blastn tool
-# the input is an R sequence and output is tabular object
+# the input is an R sequence and output is text object
 ##########################################################
 myblastn <- function(myseq,db) {
+  mytmpfile1<-tempfile()
+  mytmpfile2<-tempfile()
+  write.fasta(myseq,names=attr(myseq,"name"),file.out = mytmpfile1)
+  system2(command = "/usr/bin/blastn",
+          args = paste("-db ", db ," -query", mytmpfile1 ,"-outfmt 1 -evalue 0.05 -ungapped >" 
+                       , "tmp"))
+  # probably need to add headers here
+  res <- NULL
+  if (file.info("tmp")$size > 0 ) {
+    res <- readLines("tmp")
+  }
+  #unlink(c(mytmpfile1,mytmpfile2))
+  #res <- res[order(-res$bitscore),]
+  res
+}
+
+
+##########################################################
+# myblastn_tab is a wrapper for the unix blastn tool
+# the input is an R sequence and output is tabular object
+##########################################################
+myblastn_tab <- function(myseq,db) {
   mytmpfile1<-tempfile()
   mytmpfile2<-tempfile()
   write.fasta(myseq,names=attr(myseq,"name"),file.out = mytmpfile1)
@@ -16,12 +38,14 @@ myblastn <- function(myseq,db) {
   # probably need to add headers here
   res <- NULL
   if (file.info(mytmpfile2)$size > 0 ) {
-    res <- read.csv(mytmpfile2,sep="\t",header=FALSE)
+  res <- read.csv(mytmpfile2,sep="\t",header=FALSE)
     colnames(res) <- c("qseqid","sseqid","pident","length","mismatch","gapopen",
       "qstart","qend","sstart","send","evalue","bitscore")
   }
   unlink(c(mytmpfile1,mytmpfile2))
-  res <- res[order(-res$bitscore),]
+  if (!is.null(res)  ) {
+    res <- res[order(-res$bitscore),]
+  }
   res
 }
 
